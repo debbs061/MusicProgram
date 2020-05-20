fcc698193edb97d8404fc5f544ba2d0a9a2cdc26
kingkongs_ <gyr7220@gmail.com>
2020-05-21 04:34:35 +0900

    세팅 관련 파일 삭제

---
+++ src/main/java/com/exe/dto/TestDTO.java
@@ -0,0 +1,16 @@
+package com.exe.dto;
+
+public class TestDTO {
+	
+	private String site_name;
+
+	public String getSite_name() {
+		return site_name;
+	}
+
+	public void setSite_name(String site_name) {
+		this.site_name = site_name;
+	}
+	
+	
+}
