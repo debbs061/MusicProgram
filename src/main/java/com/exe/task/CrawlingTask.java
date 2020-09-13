ed4a57b44b6b3bd8d8651a18ba155b7544089364
yelim <>
2020-09-13 12:03:00 +0900

    매일 새벽 2시마다 각 사이트 크롤링하여 db 에 넣는 클래스 추가

---
+++ src/main/java/com/exe/task/CrawlingTask.java
@@ -0,0 +1,36 @@
+package com.exe.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.exe.dao.AlbumDAO;
+import com.exe.dao.ChartDAO;
+import com.exe.dao.SingerDAO;
+import com.exe.dao.SongDAO;
+import com.exe.service.FloCrawlingService;
+import com.exe.service.ItunesCrawlingService;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Component
+public class CrawlingTask {
+	
+	@Autowired
+	ItunesCrawlingService ics;
+	
+	@Autowired
+	FloCrawlingService fcs; 
+	
+	/*
+	 * 매일 새벽 2시마다 각 사이트 크롤링 한다.
+	 */
+	@Scheduled(cron="* * 2 * * *")
+	public void checkFiles()throws Exception{
+		ics.ItunesChartCrawling();
+		fcs.FloChartCrawling();
+	}
+
+}
