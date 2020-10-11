7287f5a8c6c7116695bad2d88273940fa163b7c3
yelim <yelimkoo@localhost>
2020-10-12 03:39:47 +0900

    3:45 분 마다 크롤

--- src/main/java/com/exe/task/CrawlingTask.java
+++ src/main/java/com/exe/task/CrawlingTask.java
@@ -27,7 +27,7 @@ public class CrawlingTask {
 	/*
 	 * 매일 새벽 2시마다 각 사이트 크롤링 한다.
 	 */
-	@Scheduled(cron="0 30 3 * * *")
+	@Scheduled(cron="0 45 3 * * *")
 	public void checkFiles()throws Exception{
 		ics.ItunesChartCrawling();
 		fcs.FloChartCrawling();
